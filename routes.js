48b15dbdb9e379e4740d7587463f987b472ed839
Juan Alfaro <franciscoalfaro2008@gmail.com>
2016-11-29 02:21:17 +0000

    added maps and announcements test route

--- routes.js
+++ routes.js
@@ -1,3 +1,30 @@
+const announcements = [
+  {
+    title: "SuchAnnounce",
+    content: "Muchcontents"
+    date: "2016-10-11 12:12:12"
+  },
+  {
+    title: "SuchAnnounce2",
+    content: "Muchcontents2"
+    date: "2016-10-11 15:12:12"
+  }
+];
+
+const maps = [
+  {
+    label: "thebestmap",
+    mapUrl: "http://www.suchurl.com"
+    int: "1"
+  },
+  {
+    label: "thewortmap",
+    mapUrl: "http://www.suchurl2.com"
+    int: "2"
+  }
+]
+
+
 const routes = [
   {
     method: 'GET',
@@ -7,6 +34,20 @@ const routes = [
         path: 'public',
         defaultExtension: 'html'
       }
+    }
+  },
+  {
+    method: 'GET',
+    path: '/announcements',
+    handler: (request,reply) => {
+      reply(announcements)
+    }
+  },
+  {
+    method: 'GET',
+    path: '/maps',
+    handler: (request,reply) => {
+      reply(maps)
     }
   }
 ]
